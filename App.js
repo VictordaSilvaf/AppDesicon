4bd9200949da4dafc39943d5cbed6b84494ad3cc
Victor da Silva Fernandes <60992300+VictordaSilvaf@users.noreply.github.com>
2021-09-14 14:03:22 -0300

    aplicativo

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      source={{ uri: 'https://www.desicon.com.br/' }}
+      style={{ marginTop: 20 }}
+    />
   );
 }
 
